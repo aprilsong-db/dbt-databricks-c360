d8f819573356bc7f722c2ff3f8edfb4421b79a96
April Song <april.song@databricks.com>
2023-07-19 15:04:07 -0700

    working version

--- dags/00_autoloader_ingest.py
+++ dags/00_autoloader_ingest.py
@@ -10,7 +10,7 @@ import os
 
 # We're hardcoding this value here for the purpose of the demo, but in a production environment this
 # would probably come from a config file and/or environment variables!
-DBT_PROJECT_DIR = "/dbt-databricks-c360/dbt"
+DBT_PROJECT_DIR = "/opt/airflow/dbt"
 
 
 with DAG(
